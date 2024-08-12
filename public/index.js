d0868f8fcf5be47301cc1564e048da54d97de05a
Saymon05 <95755350+Saymon05@users.noreply.github.com>
2024-08-12 19:18:46 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -4,25 +4,14 @@ import { checkSeExisteRegistro } from './registro/registro.js';
 const linkLogin = document.getElementById('link_login');
 
 if (await checkSeExisteRegistro()) {
-
     if (await checkLogin()) {
-
         linkLogin.href = './logout/logout.html';
-
         linkLogin.innerText = 'Logout';
-
     } else {
-
         linkLogin.href = './login/login.html';
-
         linkLogin.innerText = 'Login';
-
     }
-
 } else {
-
     linkLogin.href = './registro/registro.html';
-
     linkLogin.innerText = 'Registrar';
-
 }
