fb4642c5252c05190ab4584013cbd88f7c401dbd
Saymon05 <95755350+Saymon05@users.noreply.github.com>
2024-08-12 18:56:25 -0300

    Update base.js

--- public/url/base.js
+++ public/url/base.js
@@ -1,4 +1,4 @@
-const urlBaseAPI = 'http://localhost:3000';
-const urlBaseFront = 'http://localhost:3000';
+const urlBaseAPI = 'https://atividade8-render.onrender.com';
+const urlBaseFront = 'https://atividade8-render.onrender.com';
 
 export { urlBaseAPI, urlBaseFront };
