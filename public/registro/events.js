c90474c3e58221622e24c42fc8f7dff880d1ba63
Saymon05 <95755350+Saymon05@users.noreply.github.com>
2024-08-12 19:04:53 -0300

    Update events.js

--- public/registro/events.js
+++ public/registro/events.js
@@ -1,7 +1,8 @@
-import { realizaLogin } from './login.js';
+import { realizaRegistro } from './registro.js';
 
 const btLogin = document.querySelector('form button');
 btLogin.addEventListener('click', (e) => {
     e.preventDefault();
+    realizaRegistro();
     realizaLogin();
 });
